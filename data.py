43b7a1243fbcd0dc463bbcf2f252938e60738ff2
Anna Sakurai <anna.sakurai@futurice.com>
2023-01-26 16:21:14 +0100

    Add sort api for hp

--- data.py
+++ data.py
@@ -13,19 +13,19 @@ for row in rows:
     digimon = {}
     # Separate out the data
     number = info[0]
-    digimon['number'] = info[0]
+    digimon['number'] = int(info[0])
     digimon['digimon'] = info[1]
     digimon['stage'] = info[2]
     digimon['type'] = info[3]
     digimon['attribute'] = info[4]
-    digimon['memory'] =info[5]
-    digimon['equipSlots'] =info[6]
-    digimon['lv50HP'] = info[7]
-    digimon['lv50SP'] = info[8]
-    digimon['lv50Atk'] = info[9]
-    digimon['lv50Def'] = info[10]
-    digimon['lv50Int'] = info[11]
-    digimon['lv50Spd'] = info[12]
+    digimon['memory'] = int(info[5])
+    digimon['equipSlots'] = int(info[6])
+    digimon['lv50HP'] = int(info[7])
+    digimon['lv50SP'] = int(info[8])
+    digimon['lv50Atk'] = int(info[9])
+    digimon['lv50Def'] = int(info[10])
+    digimon['lv50Int'] = int(info[11])
+    digimon['lv50Spd'] = int(info[12])
     
     # Populate the dictionary
     digimonData[number] = digimon
--- main.py
+++ main.py
@@ -1,14 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from data import digimonData
+from typing import Optional
 import random
+import operator
 
 
 app = FastAPI()
 
+#GET / -> Ger en lista med alla Digimons
 @app.get("/")
 async def root():
     return digimonData
 
+
+
+# GET /?sort={hp|sp|defence|attack|intelligence|speed}:{desc|asc} -> Sorted list of Digimons, optionally specify ordering by appending `:asc` for ascending or `:desc` for descending. You decide on the default :slightly_smiling_face:
+
+@app.get("/sort")
+async def sorting(sort: Optional[str] = None):
+    
+    list = {}
+    for digimon_id in digimonData:
+        list[digimon_id] = digimonData[digimon_id]['lv50HP']
+    
+    print(f"list : {list}")
+    sorted_list = sorted(list.items(), key=operator.itemgetter(1))
+    
+    index = []
+    for item in sorted_list:
+        index.append(item[0])
+        
+    print(f"index list : {index}")
+
+    sorted_digimons = {}
+    for number in index:
+        print(f'number is : {number}')
+        sorted_digimons[number] = digimonData[number]
+    
+    print(f'sorted digimons is : {sorted_digimons}') 
+    return sorted_digimons
+
+
+@app.get("/get-by-name")
+def get_digimon(name: Optional[str] = None):
+    for digimon_id in digimonData:
+        if digimonData[digimon_id]["digimon"] == name:
+            return digimonData[digimon_id]
+    return {"Data": "Not found"}
+
+
+
+# Getting a specific Digimon
+# GET /:number -> Ger Digimon med det numret om den finns, 404 annars
+@app.get("/{digimon_id}", status_code=status.HTTP_200_OK)
+async def specific_digimon(digimon_id):
+    if int(digimon_id) < 1 or int(digimon_id) > 249:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        
+    return digimonData[digimon_id]
+
+
+# Getting a random Digimon
+# GET /random
 @app.get("/random")
 async def random_digimon():
     num = random.randint(1, 249)
@@ -21,14 +74,9 @@ async def random_digimon():
 
 
 # Listing Digimons
-#GET / -> Ger en lista med alla Digimons
-# GET /?sort={hp|sp|defence|attack|intelligence|speed}:{desc|asc} -> Sorted list of Digimons, optionally specify ordering by appending `:asc` for ascending or `:desc` for descending. You decide on the default :slightly_smiling_face:
+
 # GET /?stage=…                                                                                   -> Returns all Digimons of a certain stage
 # GET /?type=…                                                                                     -> Returns all Digimons of a certain type
 # GET /?attribute=…                                                                              -> Returns all Digimons of a certain attribute
 
-# Getting a specific Digimon
-# GET /:number -> Ger Digimon med det numret om den finns, 404 annars
 
-# Getting a random Digimon
-# GET /random
